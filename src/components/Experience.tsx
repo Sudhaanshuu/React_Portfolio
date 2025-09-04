3e2146c206c23d48836233492d9955cf06dce4fd
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2025-09-04 21:33:10 +0530

    fix build issues

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { Briefcase, Calendar, MapPin } from 'lucide-react';
+import {  Calendar, MapPin } from 'lucide-react';
 
 export default function Experience() {
   const experiences = [
